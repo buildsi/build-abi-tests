2ca22cd335cc79cf429c71876ed81f6bad09a2bc
Ben Woodard <woodard@redhat.com>
2021-06-24 15:38:32 -0700

    Make kinds-of-fails more specific
    This makes the kinds-of-fails script more specific so that you can sort
    through the failures more easily.

--- scripts/abidw-kinds-of-fails.sh
+++ scripts/abidw-kinds-of-fails.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 echo  "     `grep -l "code 01" *.bt | wc -l ` Consistency mismatches"
+grep -l "code 01" *.bt | xargs grep abidiff | sed -e 's/.* //' | while read lib;do
+    echo -n "    $lib from "
+    rpm -qf $lib
+done
 echo Crashes
-grep -h "#0" *.bt | sed -e 's/.*at //' | sort | uniq -c | sort -rn
+grep -h "#0" *.bt | sed -e 's/.*at //' | sort | grep -v raise.c:49 | uniq -c | sort -rn
 echo Asserts
-grep -l raise.c:49 *.bt | xargs grep "#4 " | sed -e 's/.* //' | sort | uniq -c
+grep -l raise.c:49 *.bt | xargs grep "#4 " | sed -e 's/.* //' | sort | uniq | while read loc;do
+    echo $loc
+    grep -l $loc *.bt | xargs grep abidiff | sed -e 's/.* //' | while read lib;do
+	echo -n "    $lib from "
+	rpm -qf $lib
+    done
+    echo
+done
